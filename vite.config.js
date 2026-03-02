fcd57e10ba9e95ede7114ca3b10fca114bdd03dc
Mer <maria.alvarezconde@gmail.com>
2026-03-02 17:49:30 +0100

    Actualizacion nombre repositorio

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/Frontend-Repositorio-Proyectos/',
+  base: '/Repositorio-GDA3DIV/',
   plugins: [
     react(),
     tailwindcss(),
